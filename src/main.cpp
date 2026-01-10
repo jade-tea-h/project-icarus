bb0d11b40b211b61ba82b93dc3795d5b11fece82
Abraham Fisch <abraham.f000@gmail.com>
2026-01-10 11:22:41 -0600

    Change blinks to every second

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,11 @@ void setup() {
 void loop() {
     Serial.println("ON");
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
+    delay(750);
 
     Serial.println("OFF");
     digitalWrite(LED_BUILTIN, LOW);
-    delay(500);
+    delay(750);
+
+
 }
