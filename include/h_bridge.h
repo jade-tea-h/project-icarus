ff02df90ef8c45f2053770684a69869fa0fbd250
Violet Humpherys <jade.topaz@proton.me>
2026-01-14 22:10:12 -0600

    add motor interface

--- include/h_bridge.h
+++ include/h_bridge.h
@@ -1,21 +1,41 @@
 #include <Arduino.h>
+#include <math.h>
 
 class HBridgeMotor {
 public:
   enum Direction {
     FORWARD = 1,
     BACKWARD = -1,
+    STOPPED = 0,
   };
 
-  HBridgeMotor(uint8_t pin_forward, uint8_t pin_backward, uint8_t pin_pwm);
+  HBridgeMotor(uint8_t forward_pin, uint8_t backward_pin, uint8_t pwm_pin) : pin_forward(forward_pin), pin_backward(backward_pin), pin_pwm(pwm_pin),
+      current_velocity(0) {}
+
+  void setup();
+
+  const int16_t get_velocity() { return current_velocity; }
+  const float get_velocity_percentage() { return current_velocity / 255.0; }
+  const uint8_t get_speed() { return abs(current_velocity); }
+  const float get_speed_percentage() { return abs(current_velocity) / 255.0; }
+  const Direction get_direction();
+
+  void set_velocity(Direction direction, uint8_t duty_cycle);
+  void set_velocity(int16_t velocity);
+  void set_direction(Direction direction);
+  void set_speed(uint8_t duty_cycle);
+  void set_speed(float percentage);
+  void reverse();
+  void stop();
+
+  void flip_direction();
 
 private:
   uint8_t pin_forward;
   uint8_t pin_backward;
   uint8_t pin_pwm;
 
-  Direction current_direction;
-  uint8_t current_speed;
+  int16_t current_velocity;
 };
 
 class HBridge {
--- src/h_bridge.cpp
+++ src/h_bridge.cpp
@@ -1,12 +1,86 @@
 #include <Arduino.h>
+#include <math.h>
 
 #include "h_bridge.h"
 
-HBridgeMotor::HBridgeMotor(uint8_t forward_pin, uint8_t backward_pin,
-                           uint8_t pwm_pin)
-    : pin_forward(forward_pin), pin_backward(backward_pin), pin_pwm(pwm_pin),
-      current_speed(0), current_direction(FORWARD) {
-  pinMode(pin_forward, OUTPUT);
-  pinMode(pin_backward, OUTPUT);
-  pinMode(pin_pwm, OUTPUT);
+void HBridgeMotor::setup() {
+  pinMode(this->pin_forward, OUTPUT);
+  pinMode(this->pin_backward, OUTPUT);
+  pinMode(this->pin_pwm, OUTPUT);
+}
+
+const HBridgeMotor::Direction HBridgeMotor::get_direction() {
+  if (this->current_velocity > 0) {
+    return FORWARD;
+  } else if (this->current_velocity < 0) {
+    return BACKWARD;
+  } else {
+    return STOPPED;
+  }
+}
+
+void HBridgeMotor::set_velocity(int16_t velocity) {
+  this->set_speed(uint8_t(abs(velocity)));
+
+  if (velocity > 0) {
+    this->set_direction(FORWARD);
+  } else if (velocity < 0) {
+    this->set_direction(BACKWARD);
+  } else {
+    this->stop();
+  }
+}
+
+void HBridgeMotor::set_velocity(Direction direction, uint8_t duty_cycle) {
+  this->set_direction(direction);
+
+  analogWrite(this->pin_pwm, duty_cycle);
+
+  this->current_velocity = duty_cycle * direction;
+}
+
+void HBridgeMotor::set_direction(Direction direction) {
+  switch (direction) {
+  case FORWARD:
+    digitalWrite(this->pin_forward, HIGH);
+    digitalWrite(this->pin_backward, LOW);
+    break;
+  case BACKWARD:
+    digitalWrite(this->pin_backward, HIGH);
+    digitalWrite(this->pin_forward, LOW);
+    break;
+  case STOPPED:
+    digitalWrite(this->pin_backward, LOW);
+    digitalWrite(this->pin_forward, LOW);
+    break;
+  }
+
+  this->current_velocity = abs(this->current_velocity) * direction;
+}
+
+void HBridgeMotor::set_speed(uint8_t duty_cycle) {
+  analogWrite(this->pin_pwm, duty_cycle);
+
+  if (this->current_velocity == 0) {
+    this->current_velocity = duty_cycle;
+    this->set_direction(FORWARD);
+  } else {
+    this->current_velocity =
+        (this->current_velocity / abs(this->current_velocity)) * duty_cycle;
+  }
+}
+
+void HBridgeMotor::stop() {
+  digitalWrite(this->pin_forward, LOW);
+  digitalWrite(this->pin_backward, LOW);
+
+  analogWrite(this->pin_pwm, 0);
+
+  this->current_velocity = 0;
+}
+
+void HBridgeMotor::flip_direction() {
+  uint8_t temp = this->pin_forward;
+  this->pin_forward = this->pin_backward;
+  this->pin_backward = temp;
 }
